867b56f9bb1e868e423fc02f477836ae21ee50e9
フルブライト・ケネス <furuburaitokenesu@kenesu.lan>
2023-12-09 06:26:08 +0900

    added more pages

---
+++ lib/screens/game_screen/game_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFFFFFFF),
+    );
+  }
+}
--- lib/screens/title_screen/title_screen.dart
+++ lib/screens/title_screen/title_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:snowballer/providers/settings.dart';
+import 'package:snowballer/screens/game_screen/game_screen.dart';
 
 class TitleScreen extends StatelessWidget {
   const TitleScreen({super.key});
@@ -12,19 +13,40 @@ class TitleScreen extends StatelessWidget {
       child: PopScope(
         onPopInvoked: (didPop) => false,
         canPop: false,
-        child: Container(
-          color: const Color(0xFFFFFFFF),
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Snowballer',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) {
+                  return const GameScreen();
+                },
               ),
-            ],
+            );
+          },
+          child: Container(
+            color: const Color(0xFFFFFFFF),
+            child: const SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Snowballer',
+                    style: TextStyle(
+                      fontSize: 48.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Tap to start',
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
