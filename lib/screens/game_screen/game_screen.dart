f4c1d6c80c9b49ff5323e438c9dccf3181c802f0
フルブライト・ケネス <furuburaitokenesu@kenesu.lan>
2023-12-09 10:07:57 +0900

    Added player and FlameGame code

--- lib/screens/game_screen/game_screen.dart
+++ lib/screens/game_screen/game_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatelessWidget {
@@ -5,8 +7,78 @@ class GameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFFFFFFFF),
+    final snowBallerGame = SnowballerGame();
+
+    return PopScope(
+      onPopInvoked: (didPop) => false,
+      canPop: false,
+      child: Container(
+        color: const Color(0xFFFFFFFF),
+        child: Center(
+          child: AspectRatio(
+            aspectRatio: 1 / 1,
+            child: ClipRect(
+              clipBehavior: Clip.hardEdge,
+              child: GameWidget<SnowballerGame>(
+                game: snowBallerGame,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Snowballer game
+class SnowballerGame extends FlameGame {
+  SnowballerGame();
+
+  late Player player;
+
+  @override
+  final world = World();
+  late final CameraComponent cameraComponent;
+
+  @override
+  Color backgroundColor() => const Color(0x400000FF);
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'player.png',
+    ]);
+
+    cameraComponent = CameraComponent(world: world);
+
+    cameraComponent.viewfinder.anchor = Anchor.topLeft;
+    addAll([cameraComponent, world]);
+
+    player = Player(
+      position: Vector2(0, 0),
+    );
+
+    world.add(player);
+  }
+}
+
+class Player extends SpriteAnimationComponent with HasGameRef<SnowballerGame> {
+  Player({
+    required super.position,
+  }) : super(
+          size: Vector2.all(64),
+          anchor: Anchor.topLeft,
+        );
+
+  @override
+  void onLoad() {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('player.png'),
+      SpriteAnimationData.sequenced(
+        amount: 1,
+        textureSize: Vector2.all(16),
+        stepTime: 0.12,
+      ),
     );
   }
 }
